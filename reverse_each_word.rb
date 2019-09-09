6ad37240dbfc50fd16aaa62ceb9a8b46671b8226
Angel Alanis <a4alaniz@gmail.com>
2019-09-09 17:55:52 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+def reverse_each_word(sentence1) 
+  sentence1 = "Hello there, and how are you?" 
+sentence1.split(",")  do
+  reverse_each_word.each do |sentence1|
+    puts sentence1.reverse
+  end
+end
