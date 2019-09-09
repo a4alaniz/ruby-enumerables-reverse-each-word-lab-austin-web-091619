7686effab7d5ad839171dbd7e114adfe277bfba1
Angel Alanis <a4alaniz@gmail.com>
2019-09-09 18:19:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,3 +4,8 @@ new_array = array.split(" ")
   return reversed_array.join(" ")
 end
 
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.collect {|x| x.reverse!}
+  reversed_array.join(" ")
+end
